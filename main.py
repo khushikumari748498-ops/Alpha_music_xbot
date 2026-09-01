b2002088c4807ec6d39f833897c1118090bab9e4
khushikumari748498-ops <khushikumari748498@gmail.com>
2026-09-01 21:33:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import asyncio
 from pyrogram import Client, filters
 
-# Telegram Credentials
-API_ID = 2040 
+API_ID = 2040
 API_HASH = "ef9feab27d4e8a99e0bcbeb500aff112"
 BOT_TOKEN = "8021954744:AAEzgat-16tkP0kFyvfXh3a8I2LiwmAqnF0"
 
@@ -16,12 +15,16 @@ app = Client(
 
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
-    await message.reply_text("👋 Hello! I am **ALPHA MUSIC BOT**. Add me to your group and give admin rights to play music!")
+    await message.reply_text("Hello! I am **ALPHA MUSIC BOT**.")
 
 @app.on_message(filters.command("play"))
 async def play_command(client, message):
     await message.reply_text("🎵 Playing music in Voice Chat...")
 
 if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     app.run()
+
+    
     
